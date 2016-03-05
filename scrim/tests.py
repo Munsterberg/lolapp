a87d06ff29cfb49046d5480097d46235b1a9a3de
Munsterberg <jetrussell93@gmail.com>
2016-03-05 16:53:30 -0500

    Create verbose name test for Scrim model

--- scrim/tests.py
+++ scrim/tests.py
@@ -6,3 +6,6 @@ class ScrimModelTest(TestCase):
     def test_string_representation(self):
         scrim = Scrim(team_name="TEST TEAM")
         self.assertEqual(str(scrim), scrim.team_name)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Scrim._meta.verbose_name_plural), "scrims")
